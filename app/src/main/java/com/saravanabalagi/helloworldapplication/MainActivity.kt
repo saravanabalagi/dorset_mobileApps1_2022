a45ad37dc187215cd4941f8c38db525b50c71183
Saravanabalagi <saravanabalagi@hotmail.com>
2022-02-01 10:02:39 +0000

    Week 3: Kotlin Android Extensions; Import from layouts using ids

--- app/src/main/java/com/saravanabalagi/helloworldapplication/MainActivity.kt
+++ app/src/main/java/com/saravanabalagi/helloworldapplication/MainActivity.kt
@@ -4,32 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.w("mainAct", "Activity Created")
-
         val sum1 = sum(4,5)
         val sum2 = sum(5,6,7)
         Log.i("mainAct", "Sum " + sum1 + " " + sum2)
 
-        val mainTextView = findViewById<TextView>(R.id.textView)
-        val num1EditText = findViewById<EditText>(R.id.num1)
-        val num2EditText = findViewById<EditText>(R.id.num2)
-
         val addButton = findViewById<Button>(R.id.addButton)
         addButton.setOnClickListener {
-            val num1: Int = Integer.parseInt(num1EditText.text.toString())
-            val num2: Int = Integer.parseInt(num2EditText.text.toString())
+            val num1Value: Int = Integer.parseInt(num1.text.toString())
+            val num2Value: Int = Integer.parseInt(num2.text.toString())
 //            mainTextView.text = (sum1 + sum2).toString()
 
-            val sumValue: Int = sum(num1, num2)
-            mainTextView.text = sumValue.toString()
+            val sumValue: Int = sum(num1Value, num2Value)
+            textView.text = sumValue.toString()
         }
 
     }
