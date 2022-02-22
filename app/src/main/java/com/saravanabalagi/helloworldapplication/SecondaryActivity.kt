ba08d4bbbb8dd034a017750093cb4c20f2f8dac1
Saravanabalagi <saravanabalagi@hotmail.com>
2022-02-22 11:48:51 +0000

    Week 6: ListView

--- app/src/main/java/com/saravanabalagi/helloworldapplication/SecondaryActivity.kt
+++ app/src/main/java/com/saravanabalagi/helloworldapplication/SecondaryActivity.kt
@@ -2,6 +2,9 @@ package com.saravanabalagi.helloworldapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_secondary.*
 
@@ -17,5 +20,12 @@ class SecondaryActivity: AppCompatActivity() {
             val intent = Intent(this, ThirdActivity::class.java)
             startActivity(intent)
         }
+
+        countries_list_view.adapter = ArrayAdapter<String>(this, R.layout.countries_list_template, countriesStrings)
+        countries_list_view.setOnItemClickListener { adapterView, view, i, l ->
+            val text = view.findViewById<TextView>(R.id.country_text).text
+            Toast.makeText(this, "$i $l $text" , Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
