b9af07fa9cfe9378eb1cbe2c121c6c52839036c5
Saravanabalagi <saravanabalagi@hotmail.com>
2022-03-15 11:21:16 +0000

    Week 8: Load image from URL into ImageView

--- app/src/main/java/com/saravanabalagi/helloworldapplication/PostsAdapter.kt
+++ app/src/main/java/com/saravanabalagi/helloworldapplication/PostsAdapter.kt
@@ -1,14 +1,19 @@
 package com.saravanabalagi.helloworldapplication
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat.getColor
+import androidx.core.content.ContextCompat.getDrawable
 import androidx.recyclerview.widget.RecyclerView
 import com.saravanabalagi.helloworldapplication.dataTypes.Post
+import com.squareup.picasso.Picasso
+import java.io.InputStream
+import java.net.URL
 
 class PostsAdapter(private val posts: Array<Post>, private val context: Context): RecyclerView.Adapter<PostsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostsViewHolder {
@@ -25,6 +30,26 @@ class PostsAdapter(private val posts: Array<Post>, private val context: Context)
         val numLikesTextView = holder.itemView.findViewById<TextView>(R.id.num_likes)
         val heartIcon = holder.itemView.findViewById<ImageView>(R.id.like_button)
         numLikesTextView.text = post.numLikes.toString()
+
+        val imageView = holder.itemView.findViewById<ImageView>(R.id.image)
+        val dpImageView = holder.itemView.findViewById<ImageView>(R.id.dp)
+
+        if (position == 0) {
+
+//            val dpUrl = "https://cdn.pixabay.com/photo/2020/09/26/07/05/sea-5603351_960_720.jpg"
+            val dpUrl = "https://cdn.pixabay.com/photo/2022/03/01/20/58/peace-genius-7042013_960_720.jpg"
+            Picasso.get()
+                .load(dpUrl)
+                .placeholder(getDrawable(context, R.drawable.coffee)!!)
+                .error(getDrawable(context, R.drawable.ic_baseline_error_24)!!)
+                .into(dpImageView)
+
+//            This is bad! Don't network on main thread
+//            val inputStream = URL(dpUrl).openStream()
+//            val drawable = Drawable.createFromStream(inputStream, "Pixabay")
+//            dpImageView.setImageDrawable(drawable)
+
+        }
 
         holder.itemView.findViewById<ImageView>(R.id.like_button).setOnClickListener {
             post.numLikes += 1
